19fa0c95556c33652506925fa32cdc3e4aeda250
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-02-15 13:20:28 -0300

    Finalização do Projeto - TP1.4

--- TP1_4.py
+++ TP1_4.py
@@ -1,4 +1,4 @@
-# TP1.3
+# TP1.4
 # Desenvolva um programa que peça ao usuário para escolher uma operação (adição, subtração, multiplicação, divisão) e dois números, e então execute a operação escolhida com os números.
 """
 PseudoCode:
