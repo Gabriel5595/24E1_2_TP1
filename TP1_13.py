515aac7b807e10630cee5da5e8a391bd744639fa
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-02-15 21:50:37 -0300

    Finalização do Projeto - TP1_13

---
+++ TP1_13.py
@@ -0,0 +1,12 @@
+# TP1.12
+# Desenvolva um programa que verifique se uma palavra ou frase inserida pelo usuário é um palíndromo (lê-se igual de trás para frente).
+
+def Main():
+    word = input("Type a word: ")
+    
+    if word == word[::-1]:
+        print(f"The given word {word} is the same backwards ({word[::-1]}), so it is a palindrome.")
+    else:
+        print(f"The given word {word} is not the same backwards ({word[::-1]}), so it is not a palindrome.")
+
+Main()
