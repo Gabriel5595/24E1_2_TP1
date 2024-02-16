b037df4ff0356731a81b2ee675cdc6ae0ef6cc3f
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-02-15 21:33:10 -0300

    Finalização do Projeto - TP1.11

---
+++ TP1_11.py
@@ -0,0 +1,18 @@
+# TP1.10
+# Faça um programa que simule o lançamento de um dado. O usuário deve escolher quantos dados jogar e o programa deve exibir os resultados.
+
+import random
+
+def Main():
+    diceAmount = int(input("How many dices you would like to roll? "))
+    
+    results = []
+    rolls = 0
+    while rolls != diceAmount:
+        roll = random.randint(1, 6)
+        results.append(roll)
+        rolls += 1
+    
+    print(f"The results of {diceAmount} rolls was {results}")
+
+Main()
